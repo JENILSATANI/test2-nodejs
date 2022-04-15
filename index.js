0fb9a5025f87272495fd693f5d9d2b18e32df31c
XcelTec-060 <jenilsatani97809@gmail.com>
2022-04-15 14:04:29 +0530

    node

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const app = express()
 var PORT = process.env.PORT || 9900;
-var cors = require('cors')
 const mongoose = require('mongoose')
 const db = 'mongodb+srv://jenilsatani:jenilsatani123@cluster0.d2ai8.mongodb.net/upload?retryWrites=true&w=majority'
 mongoose.connect(db).then(() => {
     console.log(`connected successfully`);
 }).catch((err) => console.log(`not succesflly`))
 var bodyParser = require("body-parser");
-// const corsOptions = {
-//     origin: '*'
-// }
-app.use(cors({origin:"https://medicinesinfo.herokuapp.com/"}))
+var cors = require('cors')
+const corsOptions = {
+    origin: '*'
+}
+app.use(cors(corsOptions))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false })); 
 const userUpload = require('./api')
