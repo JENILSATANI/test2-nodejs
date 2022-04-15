3b7c34aff75235aebf55290da05e027159d2c381
XcelTec-060 <jenilsatani97809@gmail.com>
2022-04-15 14:35:46 +0530

    node

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    serviceID:"VAe223f3739b8def8db1ad886254938ac1",
+    serviceID:"VA6d149c51c0028bd760478932d6ad776c",
     accountID: "AC14d28515d5ba471eff9e19ca02d088ca",
-    authToken: "6b135e88165d4089236e5ca9fe3cad66",
+    authToken: "64486f7845679e01ca049220ca3a5880",
 }
