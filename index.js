10bec0adafd6145544e5d7201f49520f5e8dea38
XcelTec-060 <jenilsatani97809@gmail.com>
2022-04-15 12:56:41 +0530

    node

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 var PORT = process.env.PORT || 9900;
-const corsOptions = {
-    origin: '*'
-}
+var cors = require('cors')
 const mongoose = require('mongoose')
 const db = 'mongodb+srv://jenilsatani:jenilsatani123@cluster0.d2ai8.mongodb.net/upload?retryWrites=true&w=majority'
 mongoose.connect(db).then(() => {
     console.log(`connected successfully`);
 }).catch((err) => console.log(`not succesflly`))
 var bodyParser = require("body-parser");
-app.use(cors(corsOptions))
+// const corsOptions = {
+//     origin: '*'
+// }
+app.use(cors({origin:"https://medicineplus.herokuapp.com"}))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false })); 
 const userUpload = require('./api')
 app.use('/', express.static('public'))
-app.use('/', userUpload);
+app.use('/', userUpload);    
 
-app.listen(PORT, () => { console.log('be success', PORT) })
+app.listen(PORT , ()=>{console.log('be success',PORT)})
